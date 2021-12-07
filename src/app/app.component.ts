7b9a576a9a4c09d1d8eb44819a58a1e5e6cfaeb5
phanhoangnam <phanhoangnamst@gmail.com>
2021-12-07 17:12:44 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = '12';
+  name = 'Nam';
+  age = '22';
 }
